c472eb7803e3eddf4afe6d78d7ef04c6b290dd43
mins01 <mins01.lycos.co.kr@gmail.com>
2023-10-16 16:37:35 +0900

    이상형 월드컵!

--- assets/IdealWorldCup.js
+++ assets/IdealWorldCup.js
@@ -6,7 +6,13 @@ class IdealWorldCup{
         this.container = container
     }
 
+    ready(){
+        console.log('ready');
+        this.stage('ready');
+    }
+
     start(){
+        console.log('start');
         this.roundNext();
         this.stage('round');
         this.vsNext()
@@ -19,6 +25,29 @@ class IdealWorldCup{
     }
 
 
+    result(){
+        console.log('result');
+        const item = document.querySelector('.iwc-items-ready .iwc-item')
+        this.container.querySelector('.iwc-stage-result-item').appendChild(item);
+        this.motion(100);
+    }
+
+
+    reset(){
+        console.log('reset');
+        const iwc_items_ready = this.container.querySelector('.iwc-items-ready');
+        let items = [...document.querySelectorAll('.iwc-item')];
+        items.sort((a,b)=>{
+            return parseFloat(a.dataset.idx) - parseFloat(b.dataset.idx);
+        })
+        items.forEach((el)=>{
+            iwc_items_ready.appendChild(el);
+        })
+        this.container.dataset.vsSelect = 0;
+        this.container.dataset.vs = 'off'
+    }
+
+
     stage(stage){
         this.container.dataset.stage = stage;
         this.container.querySelectorAll('*[data-stage]').forEach(el => {
@@ -28,6 +57,7 @@ class IdealWorldCup{
 
 
     round(round){
+        console.log('round');
         this.container.dataset.round = round;
         this.container.querySelectorAll('*[data-round]').forEach(el => {
             el.dataset.round = round;
@@ -43,6 +73,7 @@ class IdealWorldCup{
     }
 
     vs(vs){
+        console.log('vs');
         this.container.dataset.vs = vs;
         this.container.querySelectorAll('*[data-vs]').forEach(el => {
             el.dataset.vs = vs;
@@ -56,29 +87,41 @@ class IdealWorldCup{
         if(items.length==0){}
         else if(items.length==1){ }
         else{
-            if(this.container.querySelector('.iwc-stage-vs').dataset.vs == 'on'){
+            if(this.container.dataset.vs == 'on'){
                 console.log('이미 vs-on 상태');
                 return false;
             }
-            this.container.querySelector('.iwc-stage-vs').dataset.vs = "on";
-            
+            this.container.dataset.vs = "on"; 
             this.container.querySelector('.iwc-stage-vs-item-1').appendChild(items[0])
             this.container.querySelector('.iwc-stage-vs-item-2').appendChild(items[1])
+            this.motion(100);
         }
         return len;
     }
     vsSelect(n){
+        if(this.container.dataset.vsSelect != "0"){
+            console.log('이미 선택함');
+            return false;
+        }
         this.container.dataset.vsSelect = n;
-        this.vsSelectMove();
-        const len = this.vsNext();
-        this.vsCheck(len);
+        this.container.dataset.motion = "off";
+        
+        setTimeout(() => {
+            this.vsSelectMove();
+            const len = this.vsNext();
+            this.vsCheck(len);
+            
+        }, 1000);
+        this.motion(100);
+        
     }
     vsSelectMove(){
         const n = this.container.dataset.vsSelect
         const item_1 = this.container.querySelector('.iwc-stage-vs-item-1 .iwc-item');
         const item_2 = this.container.querySelector('.iwc-stage-vs-item-2 .iwc-item');
         this.container.dataset.vsSelect = 0;
-        this.container.querySelector('.iwc-stage-vs').dataset.vs = 'off'
+        this.container.dataset.vs = 'off'
+        
         
         if(n==1){
             this.container.querySelector('.iwc-items-win').appendChild(item_1)
@@ -98,6 +141,8 @@ class IdealWorldCup{
                 this.stage('vs');
             }else{
                 this.result();
+                this.stage('result');
+
             }
             
 
@@ -121,9 +166,13 @@ class IdealWorldCup{
         })
     }
 
-    result(){
-        const item = document.querySelector('.iwc-items-ready .iwc-item')
-        this.container.querySelector('.iwc-stage-result-item').appendChild(item);
+
+
+    motion(delay){
+        this.container.dataset.motion = "off";
+        setTimeout(() => {
+            this.container.dataset.motion = "on"; 
+        }, delay);
     }
 
 
